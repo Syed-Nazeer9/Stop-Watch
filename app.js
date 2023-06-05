279f0bd51d56bb1aa9fe7e1071918bbe43a2150e
Syed-Nazeer9 <Syednazeer679@gmail.com>
2023-06-06 03:26:47 +0500

    Done

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ function stopwatch(){
         document.getElementById("sec").innerHTML = secString;
         document.getElementById("count").innerHTML = countString;
 
-        setTimeout("stopwatch()",10)
+        setTimeout("stopwatch()",7)
     }
 }
